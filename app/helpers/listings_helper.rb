f2892f426fcbe47ad3a043798689ef77849e8bb8
hey-chan <liumatthew8@gmail.com>
2021-08-07 00:46:13 +1000

    styling show page with bootstrap

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,9 @@
 module ListingsHelper
+  def image_select(listing)
+    return listing.picture if listing.picture.attached?
+
+    # If there's no pic attached, will default to using this image. Links to show.html.erb
+    return "gamecartridge.jpeg"
+    
+  end
 end
